a5903fd927fb4ecfcbbf84cac014c1d5eccf8dbb
chaitanyalaharinayudu <81281034+chaitanyalaharinayudu@users.noreply.github.com>
2023-06-15 20:40:19 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -8,9 +8,7 @@ runuser -u cn991537 -- cat /users/cn991537/.ssh/id_rsa.pub >> /users/cn991537/.s
 apt update
 apt install -y nfs-kernel-server
 mkdir /var/nfs/keys -p
-cp /users/cn991537/.ssh/id_rsa* /var/nfs/keys
+cp /users/lngo/.ssh/id_rsa* /var/nfs/keys
 chown nobody:nogroup /var/nfs/keys
-echo "/var"
+echo "/var/nfs/keys 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 systemctl restart nfs-kernel-server
-
-
